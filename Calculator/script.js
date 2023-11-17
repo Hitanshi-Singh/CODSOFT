18f74f87c47286eb329d55202ffbf876ef52c7c3
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-11-18 01:56:10 +0530

    Task 3 Done

--- Calculator/script.js
+++ Calculator/script.js
@@ -8,19 +8,24 @@ btnArray.forEach((b) => {
   b.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
       str = screen.innerHTML.replace(/x/g, "*");
-      str = eval(str);
+      try {
+        str = eval(str);
+      } catch {
+        str = "Syntax Error";
+      }
     } else if (e.target.innerHTML == "AC") {
       str = 0;
-    } else if (e.target.innerHTML == "DEL") {
-      if (screen.innerHTML.length == 1) {
+    } else if (e.target.innerHTML == "←") {
+      if (screen.innerHTML.length == 1 || str == "Syntax Error") {
         str = 0;
       } else {
         str = screen.innerHTML.substring(0, screen.innerHTML.length - 1);
       }
     } else {
-      if (str == 0) {
+      if (str == 0 || str == "Syntax Error") {
         str = "";
       }
+
       str += e.target.innerHTML;
     }
     screen.innerHTML = str;
